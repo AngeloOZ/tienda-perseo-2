c53f38fbe73543ef8acb82f6b485d3e1c7a25025
angelooz <angelo-mjz7@hotmail.com>
2023-01-26 11:30:22 -0500

    feat: api productos

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -10,8 +10,10 @@ type Data = {
 export default function (req: NextApiRequest, res: NextApiResponse) {
     switch (req.method) {
         case 'GET':
-            res.status(200).json({ name: 'GET' })
-            break;
+            if(req.query?.id){
+                return obtenerProducto(req, res);
+            }
+            return obtenerProductos(req, res);
         case 'POST':
             return registrarProducto(req, res);
         case 'PUT':
@@ -25,22 +27,51 @@ export default function (req: NextApiRequest, res: NextApiResponse) {
     }
 }
 
+const obtenerProductos = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const productos = await prisma.producto.findMany();
+
+        return res.status(200).json(productos)
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: error.message, data: error })
+    }
+}
+
+const obtenerProducto = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const { id } = req.query as { id: string };
+        const producto = await prisma.producto.findUnique({
+            where: {
+                id: Number.parseInt(id)
+            }
+        });
+
+        return res.status(200).json(producto)
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: error.message, data: error })
+    }
+}
+
 const registrarProducto = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { name, description, category, price, stock, cover, images_list, rating, status } = req.body as IProducto;
+    try {
+        const { name, description, price, category, stock, cover, images_list, rating, status } = req.body as IProducto;
 
-    const producto = await prisma.producto.create({
-        data: {
-            name,
-            description,
-            stock,
-            price,
-            category,
-            images: JSON.stringify(images_list) as string,
-            cover: cover as string,
-            status,
-            rating: Number.parseInt(rating),
-        }
-    });
+        const producto = await prisma.producto.create({
+            data: {
+                name,
+                description,
+                stock,
+                price,
+                images: JSON.stringify(images_list) as string,
+                cover: cover as string,
+                status,
+                rating: Number.parseInt(rating),
+                categoriaId: Number.parseInt(category)
+            }
+        })
 
-    return res.status(200).json({ status: 200, message: 'Producto registrado', data: producto })
+        return res.status(200).json({ status: 200, message: 'Producto registrado', data: producto })
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: error.message, data: error })
+    }
 }
