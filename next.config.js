e88e9a8303c93cca26179f62bcbd5798be4c182c
angelooz <angelo-mjz7@hotmail.com>
2023-01-30 11:53:23 -0500

    fix: limit body api

--- next.config.js
+++ next.config.js
@@ -5,9 +5,4 @@ module.exports = {
     // HOST
     HOST_API_KEY: 'https://api-dev-minimal-v4.vercel.app',
   },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb' // Set desired value here
-    }
-  }
 };
